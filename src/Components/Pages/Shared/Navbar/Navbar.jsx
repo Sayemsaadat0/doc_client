1ccadfe3a73fc3d3b2e192fac02123fa317bc94e
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-10-02 00:16:44 +0600

    banner

--- src/Components/Pages/Shared/Navbar/Navbar.jsx
+++ src/Components/Pages/Shared/Navbar/Navbar.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-    const navaItem = <>
-        <Link to='/login'>Link1</Link>
-        <Link to='/login'>Link2</Link>
-        <Link to='/login'>Link3</Link>
+    const menuItems = <>
+        <Link to='/login'>Home</Link>
+        <Link to='/login'>Appoinment</Link>
+        <Link to='/login'>About</Link>
         <Link to='/login'>Login</Link>
     </>
     return (
@@ -16,7 +16,7 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                      {navaItem}
+                      {menuItems}
                     </ul>
                 </div>
                 <Link to='/' className="normal-case text-2xl  tracking-widest font-semibold">Doctors Portal</Link>
@@ -24,7 +24,7 @@ const Navbar = () => {
             {/*  */}
             <div className=" hidden lg:flex w-full justify-end">
                 <ul className="menu menu-horizontal lg:flex gap-10 px-1">
-                    {navaItem}
+                    {menuItems}
                 </ul>
             </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#3a4256",
+          neutral: "#ECEEF2",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
   theme: {
     extend: {},
   },
