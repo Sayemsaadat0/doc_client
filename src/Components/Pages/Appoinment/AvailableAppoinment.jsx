8cfbb209ec89b37014c0892a35719ef1c4a46a1d
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-10-03 20:46:13 +0600

    having an small eror

--- src/Components/Pages/Appoinment/AvailableAppoinment.jsx
+++ src/Components/Pages/Appoinment/AvailableAppoinment.jsx
@@ -1,25 +1,40 @@
 import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { AiFillCloseCircle } from 'react-icons/ai';
 
 const AvailableAppoinment = ({ selectedDate, setSelectedDate }) => {
-    const [appoinmentOption, setAppoinmentOptions] = useState([]) 
+    const [appoinmentOption, setAppoinmentOptions] = useState([])
     const [treatment, setTreatment] = useState(null)
-    
+
+
+
+
+    // fetching data
     useEffect(() => {
         fetch('appoinmentOptions.json')
             .then(res => res.json())
             .then(data => setAppoinmentOptions(data))
     }, [])
 
-
-    const handleBookAppointment = (data) => {
+    // opening modal by clicking 
+    const handleOpenModal = (data) => {
         setTreatment(data);
         document.getElementById('my_modal_5').showModal();
     }
+    const date = format(selectedDate, 'PP');
+
+    // handle data 
+    const { register, handleSubmit, formState: { errors }, reset } = useForm();
+    const onSubmit = data => {
+        reset();
+        console.log(data);
+    };
+    console.log(errors);
 
     return (
         <div>
-            <p className=' text-primary'>Available service on : {format(selectedDate, 'PP')}</p>
+            <p className=' text-primary text-center'>Available service on : {date}</p>
 
             <div className='grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-8 px-4 py-20'>
                 {appoinmentOption.map((data, index) => (
@@ -31,7 +46,7 @@ const AvailableAppoinment = ({ selectedDate, setSelectedDate }) => {
                             <p>{data.slots.length > 0 ? data.slots[0] : 'try another day'}</p>
                             <p>{data.slots.length} {data.slots.length > 1 ? 'spaces' : 'space'} available </p>
                             <div className='flex justify-center'>
-                                <button onClick={() => handleBookAppointment(data)} className='btn btn-primary bg-gradient-to-r from-primary to-secondary   text-white w-1/2' type='submit'>BOOK APPOINTMENT</button>
+                                <button disabled={data.slots.length == 0} onClick={() => handleOpenModal(data)} className='btn btn-primary bg-gradient-to-r from-primary to-secondary   text-white w-1/2' type='submit'>BOOK APPOINTMENT</button>
                             </div>
                         </div>
                     </div>
@@ -39,18 +54,60 @@ const AvailableAppoinment = ({ selectedDate, setSelectedDate }) => {
             </div>
 
             <dialog id="my_modal_5" className="modal modal-middle">
+
                 <div className="modal-box">
-                    <h3 className="font-bold text-lg">Hello!</h3>
+
+
                     {treatment && (
                         <div>
-                            <h4>Treatment Information:</h4>
-                            <p>Treatment Name: {treatment.name}</p>
+                            <h3 className='text-xl font-semibold mb-10'>{treatment.name}</h3>
+
+                            <div >
+                                <form className='flex  flex-col gap-6 ' onSubmit={handleSubmit(onSubmit)}>
+
+                                    {/* everything is working without this  */}
+                                 {/*    <input
+                                        className='input input-bordered text-black' type="text"
+                                        placeholder={date}
+                                        {...register("date")} /> */}
+
+                                    <select {...register("slot", { required: true })} className="select select-bordered w-full ">
+                                        <option disabled selected>Select time</option>
+                                        {treatment.slots?.map((slot, index) => <option key={index}>{slot}</option>)}
+                                    </select>
+
+                                    <input
+                                        className='input input-bordered'
+                                        type="text"
+                                        placeholder="Your Full Name"
+                                        {...register("fullname", { required: true, maxLength: 80 })} />
+
+
+                                    <input
+                                        className='input input-bordered'
+                                        type="number"
+                                        placeholder="Phone No."
+                                        {...register("phone", { required: true, maxLength: 80 })} />
+
+                                    <input
+                                        className='input input-bordered'
+                                        type="email"
+                                        placeholder="Enter Email"
+                                        {...register("email", { required: true, maxLength: 80 })} />
+
+
+
+                                    <div className='flex justify-center'>
+                                        <button className='btn bg-gradient-to-r from-primary to-secondary  btn-primary w-full text-white' type='submit'>Submit</button>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
+
                     )}
-                    <p className="py-4">Press ESC key or click the button below to close</p>
                     <div className="modal-action">
                         <form method="dialog">
-                            <button className="btn" onClick={() => setTreatment(null)}>Close</button>
+                            <button className="" onClick={() => setTreatment(null)}> <AiFillCloseCircle size={30}></AiFillCloseCircle> </button>
                         </form>
                     </div>
                 </div>
