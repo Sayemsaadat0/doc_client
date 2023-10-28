40ffc38db2313ca78c9f94043ad5e67e223dd13c
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-10-28 23:04:20 +0600

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,7 @@ function App() {
 
   return (
     <div>
-      {/* this is not working */}
-      <h1>Welcome to My Website</h1>
-      <p>This website has been visited  times.</p>
-      <button>Increment Visit Count</button>
+ 
     </div>
   );
 }
